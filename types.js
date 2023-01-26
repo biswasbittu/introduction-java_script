2b1f61715ef6379b02c6989f5a5478b9e181a8b7
biswasbittu <biswasbittu7415@yahoo.com>
2023-01-26 13:05:16 +0530

    js remainder

---
+++ types.js
@@ -0,0 +1,18 @@
+var number=50;
+var name='Number';
+var fraction= 4.2;
+ 
+var is_happy=true;
+
+
+
+
+console.log(typeof number);
+console.log(typeof name)
+console.log(typeof fraction)
+console.log(typeof is_happy)
+
+var mango = 19;
+var person =7;
+
+console.log("Remainder:  " + mango%person);
