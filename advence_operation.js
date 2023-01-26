96b1ced030fe15c525cbf1c4082da1850866acba
biswasbittu <biswasbittu7415@yahoo.com>
2023-01-26 10:45:17 +0530

    advence operation

---
+++ advence_operation.js
@@ -0,0 +1,23 @@
+// price1 = 45;
+
+// price2 = 35;
+
+// addition some valu......
+
+// price1=price1+10;
+// console.log(price1)
+// addition to the same variable shorthand....
+// price1+=10;
+// console.log(price1)
+
+// price1 -=30;
+// console.log(price2)
+
+// price2 -=10;
+// console.log(price2)
+// console.log(price1)
+price1=45;
+
+// price1 -=10;
+price1/=5;
+console.log(price1)
